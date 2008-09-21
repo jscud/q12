eec568ebe88c86c5946db150a5b997a64461d3bd
me@jeffscudder.com <me@jeffscudder.com@a33988ab-fd40-0410-9ee5-63cb7169227e>
2008-09-21 02:35:53 +0000

    Adds ARC4 encryption, HTTP PUT and DELETE requests and proper handling of HTTP request headers.

--- q12.js
+++ q12.js
@@ -130,6 +130,7 @@ q12.httpRequest = function(httpVerb, data, url, headers, handler) {
     http = new ActiveXObject('Microsoft.XMLHTTP');
   }
   if (http) {
+    http.open(httpVerb, url, true);
     http.onreadystatechange = function() {
       if (http.readyState == 4) {
         handler(http);
@@ -137,9 +138,8 @@ q12.httpRequest = function(httpVerb, data, url, headers, handler) {
     };
     var propery = null;
     for (property in headers) {
-      http.setRequestHeader();
+      http.setRequestHeader(property, headers[property]);
     }
-    http.open(httpVerb, url, true);
     http.send(data);
   } else {
     throw new Error('Unable to create the HTTP request object.');
@@ -163,7 +163,13 @@ q12.post = function(data, url, headers, handler) {
   q12.httpRequest('POST', data, url, headers, handler);
 };
 
-// TODO: add put and delete functions.
+q12.put = function(data, url, headers, handler) {
+  q12.httpRequest('PUT', data, url, headers, handler);
+};
+
+q12.del = function(url, headers, handler) {
+  q12.httpRequest('DELETE', null, url, headers, handler);
+};
 
 q12.setCookie = function(name, value, days, path) {
   var expires = ''
@@ -319,7 +325,8 @@ q12.fromUrl = function(input) {
  *   takes   byte-array 'input' (16 bytes)
  *           2D byte-array key schedule 'w' (Nr+1 x Nb bytes)
  *
- *   applies Nr rounds (10/12/14) using key schedule w for 'add round key' stage
+ *   applies Nr rounds (10/12/14) using key schedule w for 'add round key' 
+ *       stage
  *
  *   returns byte-array encrypted value (16 bytes)
  */
@@ -454,25 +461,31 @@ q12.rotWord = function(w) {
 };
 
 
-// Sbox is pre-computed multiplicative inverse in GF(2^8) used in SubBytes and KeyExpansion [§5.1.1]
-q12.sbox = [0x63,0x7c,0x77,0x7b,0xf2,0x6b,0x6f,0xc5,0x30,0x01,0x67,0x2b,0xfe,0xd7,0xab,0x76,
-            0xca,0x82,0xc9,0x7d,0xfa,0x59,0x47,0xf0,0xad,0xd4,0xa2,0xaf,0x9c,0xa4,0x72,0xc0,
-            0xb7,0xfd,0x93,0x26,0x36,0x3f,0xf7,0xcc,0x34,0xa5,0xe5,0xf1,0x71,0xd8,0x31,0x15,
-            0x04,0xc7,0x23,0xc3,0x18,0x96,0x05,0x9a,0x07,0x12,0x80,0xe2,0xeb,0x27,0xb2,0x75,
-            0x09,0x83,0x2c,0x1a,0x1b,0x6e,0x5a,0xa0,0x52,0x3b,0xd6,0xb3,0x29,0xe3,0x2f,0x84,
-            0x53,0xd1,0x00,0xed,0x20,0xfc,0xb1,0x5b,0x6a,0xcb,0xbe,0x39,0x4a,0x4c,0x58,0xcf,
-            0xd0,0xef,0xaa,0xfb,0x43,0x4d,0x33,0x85,0x45,0xf9,0x02,0x7f,0x50,0x3c,0x9f,0xa8,
-            0x51,0xa3,0x40,0x8f,0x92,0x9d,0x38,0xf5,0xbc,0xb6,0xda,0x21,0x10,0xff,0xf3,0xd2,
-            0xcd,0x0c,0x13,0xec,0x5f,0x97,0x44,0x17,0xc4,0xa7,0x7e,0x3d,0x64,0x5d,0x19,0x73,
-            0x60,0x81,0x4f,0xdc,0x22,0x2a,0x90,0x88,0x46,0xee,0xb8,0x14,0xde,0x5e,0x0b,0xdb,
-            0xe0,0x32,0x3a,0x0a,0x49,0x06,0x24,0x5c,0xc2,0xd3,0xac,0x62,0x91,0x95,0xe4,0x79,
-            0xe7,0xc8,0x37,0x6d,0x8d,0xd5,0x4e,0xa9,0x6c,0x56,0xf4,0xea,0x65,0x7a,0xae,0x08,
-            0xba,0x78,0x25,0x2e,0x1c,0xa6,0xb4,0xc6,0xe8,0xdd,0x74,0x1f,0x4b,0xbd,0x8b,0x8a,
-            0x70,0x3e,0xb5,0x66,0x48,0x03,0xf6,0x0e,0x61,0x35,0x57,0xb9,0x86,0xc1,0x1d,0x9e,
-            0xe1,0xf8,0x98,0x11,0x69,0xd9,0x8e,0x94,0x9b,0x1e,0x87,0xe9,0xce,0x55,0x28,0xdf,
-            0x8c,0xa1,0x89,0x0d,0xbf,0xe6,0x42,0x68,0x41,0x99,0x2d,0x0f,0xb0,0x54,0xbb,0x16];
+// Sbox is pre-computed multiplicative inverse in GF(2^8) used in SubBytes and
+//  KeyExpansion [§5.1.1]
+q12.sbox = [0x63,0x7c,0x77,0x7b,0xf2,0x6b,0x6f,0xc5,0x30,0x01,0x67,0x2b,0xfe,
+            0xd7,0xab,0x76,0xca,0x82,0xc9,0x7d,0xfa,0x59,0x47,0xf0,0xad,0xd4,
+            0xa2,0xaf,0x9c,0xa4,0x72,0xc0,0xb7,0xfd,0x93,0x26,0x36,0x3f,0xf7,
+            0xcc,0x34,0xa5,0xe5,0xf1,0x71,0xd8,0x31,0x15,0x04,0xc7,0x23,0xc3,
+            0x18,0x96,0x05,0x9a,0x07,0x12,0x80,0xe2,0xeb,0x27,0xb2,0x75,0x09,
+            0x83,0x2c,0x1a,0x1b,0x6e,0x5a,0xa0,0x52,0x3b,0xd6,0xb3,0x29,0xe3,
+            0x2f,0x84,0x53,0xd1,0x00,0xed,0x20,0xfc,0xb1,0x5b,0x6a,0xcb,0xbe,
+            0x39,0x4a,0x4c,0x58,0xcf,0xd0,0xef,0xaa,0xfb,0x43,0x4d,0x33,0x85,
+            0x45,0xf9,0x02,0x7f,0x50,0x3c,0x9f,0xa8,0x51,0xa3,0x40,0x8f,0x92,
+            0x9d,0x38,0xf5,0xbc,0xb6,0xda,0x21,0x10,0xff,0xf3,0xd2,0xcd,0x0c,
+            0x13,0xec,0x5f,0x97,0x44,0x17,0xc4,0xa7,0x7e,0x3d,0x64,0x5d,0x19,
+            0x73,0x60,0x81,0x4f,0xdc,0x22,0x2a,0x90,0x88,0x46,0xee,0xb8,0x14,
+            0xde,0x5e,0x0b,0xdb,0xe0,0x32,0x3a,0x0a,0x49,0x06,0x24,0x5c,0xc2,
+            0xd3,0xac,0x62,0x91,0x95,0xe4,0x79,0xe7,0xc8,0x37,0x6d,0x8d,0xd5,
+            0x4e,0xa9,0x6c,0x56,0xf4,0xea,0x65,0x7a,0xae,0x08,0xba,0x78,0x25,
+            0x2e,0x1c,0xa6,0xb4,0xc6,0xe8,0xdd,0x74,0x1f,0x4b,0xbd,0x8b,0x8a,
+            0x70,0x3e,0xb5,0x66,0x48,0x03,0xf6,0x0e,0x61,0x35,0x57,0xb9,0x86,
+            0xc1,0x1d,0x9e,0xe1,0xf8,0x98,0x11,0x69,0xd9,0x8e,0x94,0x9b,0x1e,
+            0x87,0xe9,0xce,0x55,0x28,0xdf,0x8c,0xa1,0x89,0x0d,0xbf,0xe6,0x42,
+            0x68,0x41,0x99,0x2d,0x0f,0xb0,0x54,0xbb,0x16];
 
-// Rcon is Round Constant used for the Key Expansion [1st col is 2^(r-1) in GF(2^8)] [§5.2]
+// Rcon is Round Constant used for the Key Expansion 
+// [1st col is 2^(r-1) in GF(2^8)] [§5.2]
 q12.rcon = [ [0x00, 0x00, 0x00, 0x00],
              [0x01, 0x00, 0x00, 0x00],
              [0x02, 0x00, 0x00, 0x00],
@@ -486,11 +499,12 @@ q12.rcon = [ [0x00, 0x00, 0x00, 0x00],
              [0x36, 0x00, 0x00, 0x00] ]; 
 
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 /**
- * Use AES to encrypt 'plaintext' with 'password' using 'nBits' key, in 'Counter' mode of operation
- *                           - see http://csrc.nist.gov/publications/nistpubs/800-38a/sp800-38a.pdf
+ * Use AES to encrypt 'plaintext' with 'password' using 'nBits' key, in 
+ * 'Counter' mode of operation
+ * - see http://csrc.nist.gov/publications/nistpubs/800-38a/sp800-38a.pdf
  *   for each block
  *   - outputblock = cipher(counter, key)
  *   - cipherblock = plaintext xor outputblock
@@ -503,8 +517,10 @@ q12.aesEncryptCtr = function(plaintext, password, nBits) {
   // standard allows 128/192/256 bit keys
   if (!(nBits==128 || nBits==192 || nBits==256)) return '';
 	
-  // for this example script, generate the key by applying Cipher to 1st 16/24/32 chars of password; 
-  // for real-world applications, a more secure approach would be to hash the password e.g. with SHA-1
+  // for this example script, generate the key by applying Cipher to 1st 
+  // 16/24/32 chars of password; 
+  // for real-world applications, a more secure approach would be to hash the 
+  // password e.g. with SHA-1
   // no bytes in key
   var nBytes = nBits/8; 
   var pwBytes = new Array(nBytes);
@@ -513,7 +529,8 @@ q12.aesEncryptCtr = function(plaintext, password, nBits) {
   // key is now 16/24/32 bytes long
   key = key.concat(key.slice(0, nBytes-16));
 
-  // initialise counter block (NIST SP800-38A §B.2): millisecond time-stamp for nonce in 1st 8 bytes,
+  // initialise counter block (NIST SP800-38A §B.2): millisecond time-stamp for
+  // nonce in 1st 8 bytes,
   // block counter in 2nd 8 bytes
   // block size fixed at 16 bytes / 128 bits (Nb=4) for AES
   var blockSize = 16;
@@ -522,11 +539,13 @@ q12.aesEncryptCtr = function(plaintext, password, nBits) {
   // milliseconds since 1-Jan-1970
   var nonce = (new Date()).getTime();
 
-  // encode nonce in two stages to cater for JavaScript 32-bit limit on bitwise ops
+  // encode nonce in two stages to cater for JavaScript 32-bit limit on 
+  // bitwise ops
   for (var i=0; i<4; i++) counterBlock[i] = (nonce >>> i*8) & 0xff;
-  for (var i=0; i<4; i++) counterBlock[i+4] = (nonce/0x100000000 >>> i*8) & 0xff; 
+  for (var i=0; i<4; i++) counterBlock[i+4] = (nonce/0x100000000 >>> i*8)&0xff; 
 
-  // generate key schedule - an expansion of the key into distinct Key Rounds for each round
+  // generate key schedule - an expansion of the key into distinct Key Rounds 
+  // for each round
   var keySchedule = q12.keyExpansion(key);
 
   var blockCount = Math.ceil(plaintext.length/blockSize);
@@ -534,18 +553,20 @@ q12.aesEncryptCtr = function(plaintext, password, nBits) {
   var ciphertext = new Array(blockCount);
   
   for (var b=0; b<blockCount; b++) {
-    // set counter (block #) in last 8 bytes of counter block (leaving nonce in 1st 8 bytes)
+    // set counter (block #) in last 8 bytes of counter block 
+    // (leaving nonce in 1st 8 bytes)
     // again done in two stages for 32-bit ops
     for (var c=0; c<4; c++) counterBlock[15-c] = (b >>> c*8) & 0xff;
     for (var c=0; c<4; c++) counterBlock[15-c-4] = (b/0x100000000 >>> c*8)
 
-    var cipherCntr = q12.cipher(counterBlock, keySchedule);  // -- encrypt counter block --
-    
+    // encrypt counter block
+    var cipherCntr = q12.cipher(counterBlock, keySchedule);
     // calculate length of final block:
-    var blockLength = b<blockCount-1 ? blockSize : (plaintext.length-1)%blockSize+1;
+    var blockLength=b<blockCount-1?blockSize:(plaintext.length-1)%blockSize+1;
 
     var ct = '';
-    for (var i=0; i<blockLength; i++) {  // -- xor plaintext with ciphered counter byte-by-byte --
+    for (var i=0; i<blockLength; i++) {  
+      // -- xor plaintext with ciphered counter byte-by-byte --
       var plaintextByte = plaintext.charCodeAt(b*blockSize+i);
       var cipherByte = plaintextByte ^ cipherCntr[i];
       ct += String.fromCharCode(cipherByte);
@@ -561,13 +582,15 @@ q12.aesEncryptCtr = function(plaintext, password, nBits) {
   for (var i=0; i<8; i++) ctrTxt += String.fromCharCode(counterBlock[i]);
   ctrTxt = q12.escCtrlChars(ctrTxt);
 
-  // use '-' to separate blocks, use Array.join to concatenate arrays of strings for efficiency
+  // use '-' to separate blocks, use Array.join to concatenate arrays of 
+  // strings for efficiency
   return ctrTxt + '-' + ciphertext.join('-');
 };
 
 
 /** 
- * Use AES to decrypt 'ciphertext' with 'password' using 'nBits' key, in Counter mode of operation
+ * Use AES to decrypt 'ciphertext' with 'password' using 'nBits' key, in 
+ * Counter mode of operation
  *
  *   for each block
  *   - outputblock = cipher(counter, key)
@@ -578,7 +601,8 @@ q12.aesDecryptCtr = function(ciphertext, password, nBits) {
     return '';
   }
   
-  if (!(nBits==128 || nBits==192 || nBits==256)) return '';  // standard allows 128/192/256 bit keys
+  // standard allows 128/192/256 bit keys
+  if (!(nBits==128 || nBits==192 || nBits==256)) return '';  
 
   var nBytes = nBits/8;  // no bytes in key
   var pwBytes = new Array(nBytes);
@@ -589,10 +613,12 @@ q12.aesDecryptCtr = function(ciphertext, password, nBits) {
 
   var keySchedule = q12.keyExpansion(key);
 
-  ciphertext = ciphertext.split('-');  // split ciphertext into array of block-length strings 
+  // split ciphertext into array of block-length strings 
+  ciphertext = ciphertext.split('-');  
 
   // recover nonce from 1st element of ciphertext
-  var blockSize = 16;  // block size fixed at 16 bytes / 128 bits (Nb=4) for AES
+  // block size fixed at 16 bytes / 128 bits (Nb=4) for AES
+  var blockSize = 16;
   var counterBlock = new Array(blockSize);
   var ctrTxt = q12.unescCtrlChars(ciphertext[0]);
   for (var i=0; i<8; i++) counterBlock[i] = ctrTxt.charCodeAt(i);
@@ -600,11 +626,12 @@ q12.aesDecryptCtr = function(ciphertext, password, nBits) {
   var plaintext = new Array(ciphertext.length-1);
 
   for (var b=1; b<ciphertext.length; b++) {
-    // set counter (block #) in last 8 bytes of counter block (leaving nonce in 1st 8 bytes)
+    // set counter (block #) in last 8 bytes of counter block 
+    // (leaving nonce in 1st 8 bytes)
     for (var c=0; c<4; c++) counterBlock[15-c] = ((b-1) >>> c*8) & 0xff;
-    for (var c=0; c<4; c++) counterBlock[15-c-4] = ((b/0x100000000-1) >>> c*8) & 0xff;
-
-    var cipherCntr = q12.cipher(counterBlock, keySchedule);  // encrypt counter block
+    for (var c=0; c<4; c++)counterBlock[15-c-4]=((b/0x100000000-1)>>>c*8)&0xff;
+    // encrypt counter block
+    var cipherCntr = q12.cipher(counterBlock, keySchedule);
 
     ciphertext[b] = q12.unescCtrlChars(ciphertext[b]);
 
@@ -623,29 +650,109 @@ q12.aesDecryptCtr = function(ciphertext, password, nBits) {
   return plaintext.join('');
 };
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 q12.escCtrlChars = function(str) {
   // escape control chars which might cause problems handling ciphertext
-  return str.replace(/[\0\t\n\v\f\r\xa0!-]/g, function(c) { return '!' + c.charCodeAt(0) + '!'; });
-};  // \xa0 to cater for bug in Firefox; include '-' to leave it free for use as a block marker
+  return str.replace(/[\0\t\n\v\f\r\xa0!-]/g, 
+      function(c) { return '!' + c.charCodeAt(0) + '!'; });
+};  
+// \xa0 to cater for bug in Firefox; include '-' to leave it free for use as a
+// block marker
 
 q12.unescCtrlChars = function(str) {
   // unescape potentially problematic control characters
-  return str.replace(/!\d\d?\d?!/g, function(c) { return String.fromCharCode(c.slice(1,-1)); });
+  return str.replace(/!\d\d?\d?!/g, 
+      function(c) { return String.fromCharCode(c.slice(1,-1)); });
 };
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 // Back to code written by Jeffrey Scudder
 
 q12.toAes = function(data, key) {
   return q12.aesEncryptCtr(data, key, 256);
-}
+};
 
 q12.fromAes = function(data, key) {
   return q12.aesDecryptCtr(data, key, 256);
-}
+};
+
+/**
+ * Returns an array containing the state of a new ARC4 generator.
+ */
+q12.initArc4 = function() {
+  // Initialize an array of ints 0-255 in order.
+  // This array is copied and used as a base for all keys.
+  x = [];
+  for (var i = 0; i < 256; i++) {
+    x.push(i);
+  };
+  x.push(0);
+  x.push(0);
+  return x;
+};
+
+q12.seedArc4Key = function(password) {
+  // The generated key should have 258 integers. The first 256 are the values
+  // in the key matrix (s) and the last 2 are the integer indicies for 
+  // transforming s as it is used (i and j). 
+  var s = q12.initArc4();  
+  var j = 0;
+  var temp;
+  var passwordLength = password.length;
+  for (var i = 0; i < 256; i++) {
+    j = (j + s[i] + (password.charCodeAt(i % passwordLength))) % 256;
+    temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+  }
+  return s;
+};
+
+q12.stepArc4Key = function(keyArray) {
+  var i = keyArray[256];
+  var j = keyArray[257];
+  var temp;
+  i = (i + 1) % 256;
+  j = (j + keyArray[i]) % 256;
+  temp = keyArray[i];
+  keyArray[i] = keyArray[j];
+  keyArray[j] = temp;
+  keyArray[256] = i;
+  keyArray[257] = j;
+};
+
+q12.nextArc4Number = function(keyArray) {
+  q12.stepArc4Key(keyArray);
+  var i = keyArray[256];
+  var j = keyArray[257];
+  return keyArray[(keyArray[i] + keyArray[j]) % 256];
+};
+
+/**
+ * Combines the data with a pseudo-random key stream to weakly encrypt it.
+ * To decrypt the data, use this function on the ciphertext.
+ *
+ * @param {String} data The plaintext or ciphertext to be encoded be 
+ *     combining it with a pseudo-random stream.
+ * @param {String} key A passphrase used to generate a pseudo-random key 
+ *     stream.
+ */
+q12.arc4 = function(data, key) {
+  var cipher = [];
+  var key = q12.seedArc4Key(key);
+  // Skip the first 1,000 states in each of the keys since the key state is
+  // initially pretty guessable.
+  for (var j = 0; j < 1000; j++) {
+    q12.stepArc4Key(key);
+  }
+  for (var i = 0; i < data.length; i++) {
+    cipher.push(
+        String.fromCharCode(data.charCodeAt(i) ^ q12.nextArc4Number(key)));
+  }
+  return cipher.join('');
+};
 
 /**
  * Turns an existing class into a q12 type class and inherits methods.
